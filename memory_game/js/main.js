a3313e34f00a445496d28e40d5337c6018809e14
Thekra alowais <thekra_alowais@hotmail.com>
2018-09-22 22:19:10 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,16 +2,17 @@
 
 var cards=["queen","queen","king","king"];
 var cardsInPlay=[];
-var cardOne=cards[0];
-cardsInPlay.push(cardOne);
-console.log("user flipped " + cardOne);
-var cardTwo=cards[2];
-cardsInPlay.push(cardTwo);
-console.log("user flipped " + cardTwo);
-if(cardsInPlay.length === 2){
-  if(cardOne === cardTwo){
-    alert("You found a match!");
-  }else{
-      alert("Sorry try again.");
-  }
+var checkForMatch=function(){
+    if(cardsInPlay[0] === cardsInPlay[1]){
+    console.log("You found a match!");
+      }else{
+        console.log("Sorry, try again!");
+      }
 }
+var flipCard=function(cardId){
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+}
+flipCard(0);
+flipCard(2);
